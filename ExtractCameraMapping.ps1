ca8e63f9c372a498d446d8eb80bdc2b9d2b27665
bozzybox <trevor@plainsight.cloud>
2019-10-21 08:57:26 +0200

    Add files via upload

---
+++ ExtractCameraMapping.ps1
@@ -0,0 +1,96 @@
+﻿<# 
+Script:  ExtractCameraLookup
+Purpose: Loads man.CameraMapping from text file
+Author:  Trevor Howe
+Date:    26 February 2017
+#>
+
+# Declare Variables
+$ScriptName =  $script:MyInvocation.MyCommand.Path
+$ScriptFolder = $wcgroot + "ps\"
+
+Try 
+{
+
+    # Declare Variables
+    $folder = $wcgroot + "data\" 
+    $filename = "Camera Mapping.csv"
+    $schemaname = "man"
+    $ExecutionLogKey = 0
+    $clientname = "WCG"
+    $object = "CameraMapping"
+    $auditkey = -1
+
+    # Set SQL Connection and Command Objects
+    $SqlConnection = New-Object System.Data.SqlClient.SqlConnection
+    $SqlConnection.ConnectionString = "Server= $wcgserver ;Database= $wcgstagedb ;Integrated Security=True"
+    $SqlConnection.Open()
+    $SqlCmd = New-Object System.Data.SqlClient.SqlCommand
+    $SqlCmd.Connection = $SqlConnection
+    $SqlCmd.CommandType = [System.Data.CommandType]'StoredProcedure'
+
+    # Create Execution Log
+    $SqlCmd.CommandText = "prcInsertDimExecutionLog"
+    $SqlCmd.Parameters.Clear()
+    $SqlCmd.Parameters.AddWithValue("@ScriptName",$ScriptName)
+    $OutputParm = $SQLCmd.Parameters.AddWithValue("@ExecutionLogKey",$ExecutionLogKey)
+    $OutputParm.Direction = 'Output'
+    $SqlCmd.ExecuteNonQuery()
+    $ExecutionLogKey = $OutputParm.Value
+
+    # Create Audit row 
+    $SqlCmd.CommandText = "dbo.prcInsertDimAudit"
+    $SqlCmd.Parameters.Clear()
+    $SqlCmd.Parameters.AddWithValue("@ClientName",$clientname)
+    $SqlCmd.Parameters.AddWithValue("@SchemaName",$schemaname)
+    $SqlCmd.Parameters.AddWithValue("@TableName",$object)
+    $SqlCmd.Parameters.AddWithValue("@ScriptName",$ScriptName)
+    $OutputParm = $SqlCmd.Parameters.AddWithValue("@AuditKey",$auditkey)
+    $OutputParm.Direction = 'Output'
+    $SqlCmd.ExecuteNonQuery()
+    $auditkey = $OutputParm.Value
+
+    # Execute stored procedure to extract file to table
+    $SqlCmd.CommandText = $schemaname + ".prcExtractCameraMapping"
+    $SqlCmd.Parameters.Clear()
+    $SqlCmd.Parameters.AddWithValue("@FileName",$folder + "\" + $filename)
+    $SqlCmd.ExecuteNonQuery()
+
+    # Update Audit row
+    $SqlCmd.CommandText = "dbo.prcUpdateDimAudit"
+    $SqlCmd.Parameters.Clear()
+    $SqlCmd.Parameters.AddWithValue("@AuditKey",$AuditKey)
+    $SqlCmd.ExecuteNonQuery()
+
+}
+Catch
+{
+
+    # Save Exception Details
+    $ExceptionMessage = $_.Exception.Message.Trim()
+    $ExceptionLine = $_.InvocationInfo.Line.Trim()
+    $ExectptionLineNo = $_.InvocationInfo.ScriptLineNumber
+
+    # Update Execution Log with Exception Details and Close
+    $SqlCmd.CommandText = "prcUpdateDimExecutionLog"
+    $SqlCmd.Parameters.Clear()
+    $SqlCmd.Parameters.AddWithValue("@ExecutionLogKey",$ExecutionLogKey)
+    $SqlCmd.Parameters.AddWithValue("@ExceptionLineNo",$ExectptionLineNo)
+    $SqlCmd.Parameters.AddWithValue("@ExceptionLine",$ExceptionLine)
+    $SqlCmd.Parameters.AddWithValue("@ExceptionMessage",$ExceptionMessage)
+    $SqlCmd.ExecuteNonQuery()
+
+}
+Finally
+{
+
+    # Close off Execution Log
+    $SqlCmd.CommandText = "prcUpdateDimExecutionLog"
+    $SqlCmd.Parameters.Clear()
+    $SqlCmd.Parameters.AddWithValue("@ExecutionLogKey",$ExecutionLogKey)
+    $SqlCmd.ExecuteNonQuery()
+
+    $SqlConnection.Close()
+
+}
+
