5fb018c0d5b72a7d4861831b61204f55670866f2
Trevor Howe <trevor@plainsight.cloud>
2019-11-22 13:59:22 +0200

    Commit

---
+++ PowerBIDB1/model/Tables/_Device History.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [model].[_Device History] (
+    [DeviceEventKey]         INT           NOT NULL,
+    [DeviceKey]              INT           NOT NULL,
+    [EventDateKey]           INT           NOT NULL,
+    [EventTimeKey]           INT           NOT NULL,
+    [GeoLocationKey]         INT           NOT NULL,
+    [OperationKey]           INT           NOT NULL,
+    [ShiftKey]               INT           NOT NULL,
+    [TrafficCentreKey]       INT           NOT NULL,
+    [TrafficControlEventKey] INT           NOT NULL,
+    [UserKey]                INT           NOT NULL,
+    [EventDateTime]          DATETIME      NULL,
+    [EventStatus]            VARCHAR (10)  NULL,
+    [EventValue]             VARCHAR (100) NULL,
+    [DeltaLogKey]            INT           NULL,
+    [DurationSeconds]        INT           NULL
+);
+
