c68a78343c6d17465b69c7ca4e42651d6f557742
Trevor Howe <trevor@plainsight.cloud>
2019-12-14 09:56:44 +0200

    Commit - 14 December 2019

---
+++ WCG_Stage/dbo/Views/LoadFactDeviceHistory_1.sql
@@ -0,0 +1,53 @@
+﻿
+
+CREATE VIEW [dbo].[LoadFactDeviceHistory] 
+
+AS
+
+--------------------------------------------------------------------------------------------------------------------------------------
+-- Author				:	Trevor Howe
+-- Date Created			:	15-11-2019
+-- Reason				:	Load view for FactDeviceHistory
+--------------------------------------------------------------------------------------------------------------------------------------
+-- Modified By			:	
+-- Modified On			:	
+-- Reason				:	
+--------------------------------------------------------------------------------------------------------------------------------------
+
+SELECT
+	ISNULL(d1.DeviceKey,-1)										AS DeviceKey
+	,ISNULL(d9.DeviceEventKey,-1)								AS DeviceEventKey
+	,ISNULL(d2.DateKey,-1)										AS EventDateKey
+	,ISNULL(d3.TimeKey,-1)										AS EventTimeKey
+	--GeoLocationKey
+	,ISNULL(d4.OperationKey,-1)									AS OperationKey
+	,ISNULL(d5.ShiftKey,-1)										AS ShiftKey
+	,ISNULL(d6.TrafficCentreKey,-1)								AS TrafficCentreKey
+	,ISNULL(d7.TrafficControlEventKey,-1)						AS TrafficControlEventKey
+	,ISNULL(d8.UserKey,-1)										AS UserKey
+	-- Possible Dimensions Later
+	,tfm.LatitudeRange											AS Latitude
+	,tfm.LongitudeRange											AS Longitude
+	,tfm.EventSource											AS EventSource
+	,tfm.EventSubType											AS EventSubType
+	,tfm.EventType												AS EventType
+	-- Degenerate Dimensions
+	,tfm.EventDateTime											AS EventDateTime
+	,tfm.EventStatus											AS EventStatus
+	,tfm.EventValue												AS EventValue
+	,tfm.UniqueID												AS UniqueID
+	,ROW_NUMBER() OVER (
+					PARTITION BY
+					tfm.UniqueId
+					ORDER BY d7.TrafficControlEventKey DESC)	AS RowSequence
+	,tfm.[DeltaLogKey]											AS DeltaLogKey
+FROM [WCG_Stage].[itis].[transformFactDeviceHistory] tfm
+LEFT JOIN WCG_DW.dbo.DimDevice					d1 ON tfm.DeviceID = d1.DeviceId
+LEFT JOIN WCG_DW.dbo.DimDate					d2 ON tfm.EventDate = d2.FullDate
+LEFT JOIN WCG_DW.dbo.DimTime					d3 ON tfm.EventTime = d3.FullTime
+LEFT JOIN WCG_DW.dbo.DimOperation				d4 ON tfm.OperationID = d4.OperationID
+LEFT JOIN WCG_DW.dbo.DimShift					d5 ON tfm.ShiftID = d5.ShiftID
+LEFT JOIN WCG_DW.dbo.DimTrafficCentre			d6 ON tfm.TrafficCentreID = d6.TrafficCentreID
+LEFT JOIN WCG_DW.dbo.DimTrafficControlEvent		d7 ON tfm.TrafficControlEventGUID = d7.TrafficControlEventGUID
+LEFT JOIN WCG_DW.dbo.DimUser					d8 ON tfm.UserID = d8.UserID
+LEFT JOIN WCG_DW.dbo.DimDeviceEvent				d9 ON tfm.EventSource = d9.EventSource AND tfm.EventType = d9.EventType AND tfm.EventSubType = d9.EventSubType
---
+++ WCG_Stage/itis/Views/transformFactDeviceHistory_1.sql
@@ -0,0 +1,177 @@
+﻿
+
+
+
+
+CREATE VIEW [itis].[transformFactDeviceHistory] AS
+
+--------------------------------------------------------------------------------------------------------------------------------------
+-- Author				:	Trevor Howe
+-- Date Created			:	15-11-2019
+-- Reason				:	Transform view for FactDeviceHistory from itis.last_known_location
+--------------------------------------------------------------------------------------------------------------------------------------
+-- Modified By			:	
+-- Modified On			:	
+-- Reason				:	
+----------------------------------------------------------------------------------------------------------------------------------------
+
+--WITH OperationCTE AS (
+--SELECT
+--	 id																		AS OperationID
+--	,CAST(
+--		CAST(actual_operation_start_time AS DATETIMEOFFSET) 
+--			AT TIME ZONE 'South Africa Standard Time' 
+--		AS DATETIME)														AS actual_operation_start_time
+--	,CAST(
+--		CAST(actual_operation_stop_time AS DATETIMEOFFSET) 
+--			AT TIME ZONE 'South Africa Standard Time' 
+--		AS DATETIME)														AS actual_operation_stop_time
+--	,ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_at DESC)			AS RowSequence
+--FROM itis.[operation]
+--WHERE 	
+--	actual_operation_start_time IS NOT NULL
+--	AND actual_operation_stop_time IS NOT NULL
+
+--)
+
+--,OperationAssignmentCTE AS (
+--SELECT
+--	 a.user_id																AS UserID
+--	,a.operation_id															AS OperationID
+--	,c.actual_operation_start_time											AS OperationStartDateTime
+--	,c.actual_operation_stop_time											AS OperationStopDateTime
+--	,ROW_NUMBER() OVER (PARTITION BY a.user_id, a.operation_id ORDER BY a.updated_at DESC) AS RowSequence
+--FROM WCG_Stage.itis.[officials_involved_in_operation] a WITH (NOLOCK)
+--INNER JOIN OperationCTE c ON a.operation_id = c.OperationID 
+--WHERE 
+--	a.user_id IS NOT NULL
+--AND c.RowSequence = 1
+--)
+
+--,ShiftCTE AS (
+--SELECT 
+--	 i.user_id																AS UserID
+--	,i.id																	AS ShiftID
+--	,CAST(s.ShiftDate AS DATETIME) + CAST(s.StartTime AS DATETIME)			AS ShiftStartDateTime
+--	,CAST(s.ShiftDate AS DATETIME) + CAST(s.EndTime AS DATETIME)			AS ShiftEndDateTime
+--	,ROW_NUMBER() OVER (
+--					PARTITION BY 
+--						i.user_id, 
+--						i.id 
+--					ORDER BY s.ShiftDate, s.EndTime)						AS RowSequence
+--FROM WCG_Stage.itis.shift i
+--LEFT JOIN WCG_Stage.itis.transformDimShift s on i.id = s.shiftID
+--WHERE 
+--	s.IsOffDuty = 'No'
+--AND i.user_id IS NOT NULL
+--)
+
+WITH OperationAssignmentDWCTE AS (
+SELECT
+	 d2.UserID																AS UserID
+	,d1.OperationID															AS OperationID
+	,d1.ActualStartTime														AS OperationStartDateTime
+	,d1.ActualStopTime														AS OperationStopDateTime
+FROM WCG_DW.dbo.FactOperationAssignments f WITH (NOLOCK)
+LEFT JOIN WCG_DW.dbo.DimOperation		d1 WITH (NOLOCK) ON f.OperationKey = d1.OperationKey
+LEFT JOIN WCG_DW.dbo.DimUser			d2 WITH (NOLOCK) ON f.UserKey = d2.UserKey
+WHERE 
+	f.OperationKey <> -1
+AND f.UserKey <> -1
+)
+
+,ShiftDWCTE AS (
+SELECT 
+	 d2.UserID																AS UserID
+	,d1.ShiftID																AS ShiftID
+	,CAST(d1.ShiftDate AS DATETIME) + CAST(d1.StartTime	AS DATETIME)		AS ShiftStartDateTime
+	,DATEADD(
+		MINUTE,
+		f.DurationHours*60,	
+		CAST(d1.ShiftDate AS DATETIME) + CAST(d1.StartTime	AS DATETIME)
+	)																		AS ShiftEndDateTime
+FROM WCG_DW.dbo.FactShiftTimes f	WITH (NOLOCK)
+LEFT JOIN WCG_DW.dbo.DimShift d1	WITH (NOLOCK) ON f.ShiftKey = d1.ShiftKey
+LEFT JOIN WCG_DW.dbo.DimUser d2		WITH (NOLOCK) ON f.UserKey = d2.UserKey
+WHERE 
+	d1.IsOffDuty = 'No'
+AND f.UserKey <> -1
+AND f.ShiftKey <> -1
+)
+
+,LastKnownLocationCTE AS (
+SELECT
+	device_id																AS DeviceID
+	,CAST(
+		CAST(user_event_date AS DATETIMEOFFSET) 
+			AT TIME ZONE 'South Africa Standard Time' 
+		AS DATE)															AS EventDate
+	,CAST(
+		CAST(user_event_date AS DATETIMEOFFSET) 
+			AT TIME ZONE 'South Africa Standard Time' 
+		AS DATETIME)														AS EventDateTime
+	,ISNULL(CAST(user_event_source AS VARCHAR(50)),'Unknown')				AS EventSource
+	,CAST(status_display AS VARCHAR(10))									AS EventStatus
+	,ISNULL(CAST(user_event_subtype AS VARCHAR(50)),'Unknown')				AS EventSubType
+	,CAST(
+		SUBSTRING(
+			CONVERT(
+				VARCHAR(20),
+				CAST(user_event_date AS DATETIMEOFFSET)	
+				AT TIME ZONE 'South Africa Standard Time',
+				113
+			),13,5
+		) AS TIME)															AS EventTime
+	,ISNULL(CAST(user_event_type AS VARCHAR(50)),'Unknown')					AS EventType
+	,CAST(user_event_information AS VARCHAR(100))							AS EventValue
+	,CAST(latitude AS NUMERIC(11,5))										AS LatitudeRange
+	,CAST(longitude AS NUMERIC(11,5))										AS LongitudeRange
+	,traffic_centre_id														AS TrafficCentreID
+	,user_event_id															AS TrafficControlEventGUID
+	,id																		AS UniqueID
+	,[user_id]																AS UserID
+	,l.updated_at															AS UpdateDateTime
+	,l.DeltaLogKey															AS DeltaLogKey
+FROM itis.transformLastKnownLocation l
+)
+
+,MainCTE AS (
+SELECT 
+	 l.*
+	,o.OperationID															AS OperationID
+	,s.ShiftID																AS ShiftID
+	,ROW_NUMBER() OVER (
+					PARTITION BY l.UniqueId 
+					ORDER BY UpdateDateTime DESC)							AS RowSequence
+from LastKnownLocationCTE l
+LEFT JOIN OperationAssignmentDWCTE o 
+	ON l.UserID = o.UserID 
+	AND l.EventDateTime BETWEEN o.OperationStartDateTime AND o.OperationStopDateTime 
+LEFT JOIN ShiftDWCTE s 
+	ON l.UserID = s.UserID 
+	AND EventDateTime BETWEEN s.ShiftStartDateTime AND s.ShiftEndDateTime 
+
+)
+
+SELECT 
+	 DeviceID
+	,EventDate
+	,EventDateTime
+	,EventSource
+	,EventStatus
+	,EventSubType
+	,EventTime
+	,EventType
+	,EventValue
+	,LatitudeRange
+	,LongitudeRange
+	,OperationID
+	,ShiftID
+	,TrafficCentreID
+	,TrafficControlEventGUID
+	,UniqueID
+	,UserID
+	,DeltaLogKey
+FROM MainCTE
+WHERE 
+	RowSequence = 1
